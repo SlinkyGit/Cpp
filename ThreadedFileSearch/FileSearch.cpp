2d36027a4ba22daa67cefb553530595345d7fad6
Nikhil Solanki <nikhil.u.solanki@gmail.com>
2024-06-17 15:02:59 -0400

    Update FileSearch.cpp

--- ThreadedFileSearch/FileSearch.cpp
+++ ThreadedFileSearch/FileSearch.cpp
@@ -1,9 +1,6 @@
 /**
  * Nikhil Solanki
- * CS 361
- * HW 7
  * Please refer to code comments and citations as necessary.
- * Inspired by Professor Boady's searchTask and threadPool classes.
 */
 
 #include "FileSearch.h"
